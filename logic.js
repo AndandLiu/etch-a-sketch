beea3c2e761652a2aa357050e23525bb2ea35387
andrew <andrewliu13531@gmail.com>
2024-08-21 20:23:40 -0700

    build board

---
+++ logic.js
@@ -0,0 +1,11 @@
+function buildBoard() {
+    const container = document.querySelector("#container");
+
+    for(let i = 0; i < 256; i++) {
+        const div = document.createElement("div");
+        div.classList.add("cell");
+        container.appendChild(div)
+    }
+}
+
+buildBoard();
